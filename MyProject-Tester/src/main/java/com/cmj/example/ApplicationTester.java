eea718574667e62ff60f01568e632df434db333e
mengjie_chen <1737756104@qq.com>
2020-11-10 11:06:41 +0800

    commit

---
+++ MyProject-Tester/src/main/java/com/cmj/example/ApplicationTester.java
@@ -0,0 +1,16 @@
+package com.cmj.example;
+
+import org.junit.Test;
+
+/**
+ * @author mengjie_chen
+ * @description date 2020/11/10
+ */
+public class ApplicationTester {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
