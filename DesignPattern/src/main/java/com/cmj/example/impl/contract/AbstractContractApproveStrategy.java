a8332f6fbe36c4d642899d6d43eb94ec3eea022a
mengjie_chen <1737756104@qq.com>
2020-11-19 19:31:14 +0800

    commit

---
+++ DesignPattern/src/main/java/com/cmj/example/impl/contract/AbstractContractApproveStrategy.java
@@ -0,0 +1,53 @@
+package com.cmj.example.impl.contract;
+
+import com.cmj.example.service.contract.IContractApproveStrategy;
+import com.cmj.example.vo.ResVo;
+
+/**
+ * @author mengjie_chen
+ * @description date 2020/11/19
+ */
+public abstract class AbstractContractApproveStrategy implements IContractApproveStrategy {
+
+    protected final ResVo resVo = new ResVo();
+
+    @Override
+    public ResVo approve() {
+        if (check().isSuccess() && doInvoke().isSuccess()) {
+            save();
+        }
+        return resVo;
+    }
+
+    /**
+     * 检验参数
+     *
+     * @param
+     * @return com.cmj.example.vo.ResVo
+     * @author mengjie_chen
+     * @date 2020/11/19
+     */
+    protected abstract ResVo check();
+
+    /**
+     * 执行流程
+     *
+     * @param
+     * @return com.cmj.example.vo.ResVo
+     * @author mengjie_chen
+     * @date 2020/11/19
+     */
+    protected abstract ResVo doInvoke();
+
+    /**
+     * 保存数据
+     *
+     * @param
+     * @return com.cmj.example.vo.ResVo
+     * @author mengjie_chen
+     * @date 2020/11/19
+     */
+    protected abstract void save();
+
+
+}
