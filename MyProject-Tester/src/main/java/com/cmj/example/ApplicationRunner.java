3f5d63d6d1326ba977073a0dbefc7433ac7392b4
mengjie_chen <1737756104@qq.com>
2020-12-03 16:49:57 +0800

    重構提交訂單

---
+++ MyProject-Tester/src/main/java/com/cmj/example/ApplicationRunner.java
@@ -0,0 +1,17 @@
+package com.cmj.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mengjie_chen
+ * @description date 2020/11/10
+ */
+@SpringBootApplication
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+
+}
--- MyProject-Tester/src/test/java/java/com/cmj/example/ApplicationTester.java
+++ MyProject-Tester/src/test/java/java/com/cmj/example/ApplicationTester.java
@@ -1,4 +1,4 @@
-package com.cmj.example;
+package java.com.cmj.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
