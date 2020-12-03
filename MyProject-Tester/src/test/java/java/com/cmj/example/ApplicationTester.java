784ef7e2828d29d1a14356a7ee19b7f0c6e97716
mengjie_chen <1737756104@qq.com>
2020-12-03 21:08:03 +0800

    重構提交訂單

--- MyProject-Tester/src/test/java/java/com/cmj/example/ApplicationTester.java
+++ MyProject-Tester/src/test/java/java/com/cmj/example/ApplicationTester.java
@@ -1,17 +1,21 @@
 package java.com.cmj.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author mengjie_chen
  * @description date 2020/11/10
  */
-@SpringBootApplication
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = ApplicationTester.class)
 public class ApplicationTester {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationTester.class, args);
+    @Test
+    public void contextLoads() {
+
     }
 
 }
