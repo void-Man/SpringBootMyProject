d245f5b07e5d928eed020d2bb6dcec7e83e3cfed
mengjie_chen <1737756104@qq.com>
2020-12-17 17:18:36 +0800

    提交订单重构-参数替换

--- DesignPattern/src/main/java/com/cmj/example/chain/impl/abs/initializer/AbstractChainInitializer.java
+++ DesignPattern/src/main/java/com/cmj/example/chain/impl/abs/initializer/AbstractChainInitializer.java
@@ -1,6 +1,6 @@
 package com.cmj.example.chain.impl.abs.initializer;
 
-import com.cmj.example.chain.service.InterceptorInitializer;
+import com.cmj.example.chain.service.ChainInitializer;
 import com.cmj.example.chain.service.OrderInvokeHandler;
 import com.cmj.example.chain.service.OrderSubmitChain;
 import com.cmj.example.vo.OrderResultVo;
@@ -13,16 +13,24 @@ import java.util.List;
  * @author mengjie_chen
  * @description date 2020/11/24
  */
-public abstract class AbstractInterceptorInitializer<T extends OrderSubmitChain<E>, E extends SubmitOrderBaseParamVo> implements InterceptorInitializer<E> {
+public abstract class AbstractChainInitializer<T extends OrderSubmitChain<E>, E extends SubmitOrderBaseParamVo> implements ChainInitializer<T, E> {
     private final List<T> validatorList = new ArrayList<>(10);
 
-    public AbstractInterceptorInitializer<T, E> addLast(T validator) {
+    @Override
+    public ChainInitializer<T, E> addLast(T validator) {
         validatorList.add(validator);
         return this;
     }
 
-    @Override
-    public void check(E param) {
+    /**
+     * 检验参数
+     *
+     * @param param
+     * @return void
+     * @author mengjie_chen
+     * @date 2020/12/17
+     */
+    private void validate(E param) {
         for (T checker : validatorList) {
             checker.check(param);
         }
@@ -30,6 +38,7 @@ public abstract class AbstractInterceptorInitializer<T extends OrderSubmitChain<
 
     @Override
     public OrderResultVo invoke(E param) {
+        this.validate(param);
         for (T checker : validatorList) {
             OrderInvokeHandler<E> handler = checker.create();
             handler.invoke(param);
--- DesignPattern/src/main/java/com/cmj/example/tester/MyTest.java
+++ DesignPattern/src/main/java/com/cmj/example/tester/MyTest.java
@@ -3,8 +3,8 @@ package com.cmj.example.tester;
 import com.cmj.example.chain.impl.LimitBuySubmitChain;
 import com.cmj.example.chain.impl.MerchantSubmitChain;
 import com.cmj.example.chain.impl.abs.interceptor.AbstractDefaultOrderSubmitChain;
-import com.cmj.example.chain.impl.abs.initializer.AbstractInterceptorInitializer;
-import com.cmj.example.chain.impl.DefaultSubmitOrderInterceptorInitializer;
+import com.cmj.example.chain.impl.DefaultSubmitOrderChainInitializer;
+import com.cmj.example.chain.service.ChainInitializer;
 import com.cmj.example.vo.DefaultSubmitOrderParamVo;
 import org.junit.Test;
 
@@ -16,8 +16,8 @@ public class MyTest {
 
     @Test
     public void test1() {
-        DefaultSubmitOrderInterceptorInitializer defaultSubmitOrderInterceptorInitializer = new DefaultSubmitOrderInterceptorInitializer();
-        AbstractInterceptorInitializer<AbstractDefaultOrderSubmitChain, DefaultSubmitOrderParamVo> initializer = defaultSubmitOrderInterceptorInitializer
+        DefaultSubmitOrderChainInitializer defaultSubmitOrderInterceptorInitializer = new DefaultSubmitOrderChainInitializer();
+        ChainInitializer<AbstractDefaultOrderSubmitChain, DefaultSubmitOrderParamVo> initializer = defaultSubmitOrderInterceptorInitializer
                 .addLast(new LimitBuySubmitChain())
                 .addLast(new MerchantSubmitChain());
         DefaultSubmitOrderParamVo defaultSubmitOrderParamVo = new DefaultSubmitOrderParamVo();
