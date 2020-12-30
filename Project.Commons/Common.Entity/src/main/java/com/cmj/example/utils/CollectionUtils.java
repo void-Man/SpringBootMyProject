497f85c64aae3a47e3a495271c729d6fdf52c7c6
mengjie_chen <1737756104@qq.com>
2020-12-31 00:08:22 +0800

    服务stock数据完成

--- Project.Commons/Common.Entity/src/main/java/com/cmj/example/utils/CollectionUtils.java
+++ Project.Commons/Common.Entity/src/main/java/com/cmj/example/utils/CollectionUtils.java
@@ -1,6 +1,8 @@
 package com.cmj.example.utils;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -34,4 +36,14 @@ public class CollectionUtils {
         return Objects.nonNull(collection) || collection.size() > 0;
     }
 
+    public static <T> List<List<T>> subList(List<T> sourceList, int batchSize) {
+        List<List<T>> list =  new ArrayList<>(10);
+        int sourceSize = sourceList.size();
+        int batch = sourceSize / batchSize + (sourceSize % batchSize == 0 ? 0 : 1);
+        for (int i = 0; i < batch; i++) {
+            List<T> subList = sourceList.subList(i * batchSize, (i == batch - 1) ? sourceSize : i * batchSize + batchSize);
+            list.add(subList);
+        }
+        return list;
+    }
 }
--- Project.Service/FundService/src/main/java/com/cmj/example/fund/StockServiceImpl.java
+++ Project.Service/FundService/src/main/java/com/cmj/example/fund/StockServiceImpl.java
@@ -1,11 +1,17 @@
 package com.cmj.example.fund;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cmj.example.base.StockBase;
 import com.cmj.example.mapper.StockBaseMapper;
+import com.cmj.example.utils.CollectionUtils;
 import com.cmj.example.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author mengjie_chen
@@ -13,7 +19,7 @@ import java.io.IOException;
  * @date 2020/12/30
  */
 @Service
-public class StockServiceImpl implements StockService{
+public class StockServiceImpl implements StockService {
 
     @Autowired
     private StockBaseMapper stockBaseMapper;
@@ -21,5 +27,31 @@ public class StockServiceImpl implements StockService{
     @Override
     public void addStock(String path) throws IOException {
         String content = FileUtils.readFile(path);
+        List<StockBase> stockBaseList = this.convertVo(content);
+//        CollectionUtils.subList(stockBaseList, 200)
+//                .forEach(stockBases -> stockBaseMapper.batchInsertSelective(stockBases, StockBase.Column.name, StockBase.Column.number));
+    }
+
+    /**
+     * 转换数据
+     *
+     * @param content
+     * @return java.util.List<com.cmj.example.base.StockBase>
+     * @author mengjie_chen
+     * @date 2020/12/30
+     */
+    private List<StockBase> convertVo(String content) {
+        JSONArray diffList = JSONObject.parseObject(content).getJSONObject("data").getJSONArray("diff");
+        List<StockBase> stockBaseList = new ArrayList<>(10);
+        for (int i = 0; i < diffList.size(); i++) {
+            JSONObject diffVo = diffList.getJSONObject(i);
+            StockBase stockBase = StockBase.builder()
+                    .number(diffVo.getString("f12"))
+                    .name(diffVo.getString("f14"))
+                    .build();
+            stockBaseMapper.insertSelective(stockBase);
+            stockBaseList.add(stockBase);
+        }
+        return stockBaseList;
     }
 }
