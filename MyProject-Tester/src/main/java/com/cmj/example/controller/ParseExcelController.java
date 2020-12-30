58faf5f8b624f38f13f9e266e24cf6e145bd921e
mengjie_chen <1737756104@qq.com>
2020-12-30 16:16:10 +0800

    基金base生成

--- MyProject-Tester/src/main/java/com/cmj/example/controller/ParseExcelController.java
+++ MyProject-Tester/src/main/java/com/cmj/example/controller/ParseExcelController.java
@@ -4,6 +4,7 @@ import com.cmj.example.base.FundEntryBase;
 import com.cmj.example.mapper.FundBaseMapper;
 import com.cmj.example.mapper.FundEntryBaseMapper;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,8 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,60 +39,56 @@ public class ParseExcelController {
 
     @PostMapping("/parseAndSave")
     @ResponseBody
-    public String parseAndSave(HttpServletRequest request) {
+    public String parseAndSave(HttpServletRequest request) throws IOException, InvalidFormatException {
 
         //excel文件路径
         String excelPath = "C:\\Users\\Safendaga\\Downloads\\新建 Microsoft Excel 工作表.xlsx";
 
-        try {
-            request.setCharacterEncoding("UTF-8");
-            File excel = new File(excelPath);
-            if (excel.isFile() && excel.exists()) {   //判断文件是否存在
+        File excel = new File(excelPath);
+        if (!excel.isFile() || !excel.exists()) {
+            return "faild";
+        }
 
-                String[] split = excel.getName().split("\\.");  //.是特殊字符，需要转义！！！！！
-                Workbook wb;
-                //根据文件后缀（xls/xlsx）进行判断
-                if ("xls".equals(split[1])) {
-                    FileInputStream fis = new FileInputStream(excel);   //文件流对象
-                    wb = new HSSFWorkbook(fis);
-                } else if ("xlsx".equals(split[1])) {
-                    wb = new XSSFWorkbook(excel);
-                } else {
-                    return "faild";
-                }
+        request.setCharacterEncoding("UTF-8");
+        String[] split = excel.getName().split("\\.");  //.是特殊字符，需要转义！！！！！
+        Workbook wb;
+        //根据文件后缀（xls/xlsx）进行判断
+        if ("xls".equals(split[1])) {
+            FileInputStream fis = new FileInputStream(excel);   //文件流对象
+            wb = new HSSFWorkbook(fis);
+        } else if ("xlsx".equals(split[1])) {
+            wb = new XSSFWorkbook(excel);
+        } else {
+            return "faild";
+        }
 
-                //开始解析
-                Sheet sheet = wb.getSheetAt(0);     //读取sheet 0
+        //开始解析
+        Sheet sheet = wb.getSheetAt(0);     //读取sheet 0
 
-                int firstRowIndex = sheet.getFirstRowNum();   //第一行是列名，所以不读
-                int lastRowIndex = sheet.getLastRowNum();
+        int firstRowIndex = sheet.getFirstRowNum();   //第一行是列名，所以不读
+        int lastRowIndex = sheet.getLastRowNum();
 
-                List<FundEntryBase> fundEntryBaseList = new ArrayList<>(10);
-                for (int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {   //遍历行
-                    System.out.println("rIndex: " + rIndex);
-                    Row row = sheet.getRow(rIndex);
-                    if (row != null) {
-                        int firstCellIndex = row.getFirstCellNum();
-                        int lastCellIndex = row.getLastCellNum();
-                        FundEntryBase fundEntryBase = new FundEntryBase();
-                        for (int cIndex = firstCellIndex; cIndex < lastCellIndex; cIndex++) {   //遍历列
-                            Cell cell = row.getCell(cIndex);
-                            if (cIndex == 2) {
-                                fundEntryBase.setStockNamer(cell.toString());
-                            }
-                            if (cIndex == 6) {
-                                fundEntryBase.setRate(new BigDecimal(cell.toString()).multiply(new BigDecimal("100")));
-                            }
-                        }
-                        fundEntryBaseList.add(fundEntryBase);
+        List<FundEntryBase> fundEntryBaseList = new ArrayList<>(10);
+        for (int rIndex = firstRowIndex; rIndex <= lastRowIndex; rIndex++) {   //遍历行
+            System.out.println("rIndex: " + rIndex);
+            Row row = sheet.getRow(rIndex);
+            if (row != null) {
+                int firstCellIndex = row.getFirstCellNum();
+                int lastCellIndex = row.getLastCellNum();
+                FundEntryBase fundEntryBase = new FundEntryBase();
+                for (int cIndex = firstCellIndex; cIndex < lastCellIndex; cIndex++) {   //遍历列
+                    Cell cell = row.getCell(cIndex);
+                    if (cIndex == 2) {
+                        fundEntryBase.setStockNamer(cell.toString());
+                    }
+                    if (cIndex == 6) {
+                        fundEntryBase.setRate(new BigDecimal(cell.toString()).multiply(new BigDecimal("100")));
                     }
                 }
-                fundEntryBaseMapper.batchInsertSelective(fundEntryBaseList, FundEntryBase.Column.stockNamer, FundEntryBase.Column.rate);
-            } else {
+                fundEntryBaseList.add(fundEntryBase);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+        fundEntryBaseMapper.batchInsertSelective(fundEntryBaseList, FundEntryBase.Column.stockNamer, FundEntryBase.Column.rate);
         return "success";
     }
 
