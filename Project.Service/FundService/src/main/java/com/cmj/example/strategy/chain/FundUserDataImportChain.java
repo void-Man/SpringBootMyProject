034de0954d15fdfed9a6799683ab0f9cbbdc437e
mengjie_chen <1737756104@qq.com>
2021-01-03 00:09:07 +0800

    基金添加逻辑重构

--- Project.Service/FundService/src/main/java/com/cmj/example/strategy/chain/FundUserDataImportChain.java
+++ Project.Service/FundService/src/main/java/com/cmj/example/strategy/chain/FundUserDataImportChain.java
@@ -2,10 +2,8 @@ package com.cmj.example.strategy.chain;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.cmj.example.base.FundTypeBase;
-import com.cmj.example.base.FundTypeBaseExample;
 import com.cmj.example.base.FundUserBase;
-import com.cmj.example.mapper.FundTypeBaseMapper;
+import com.cmj.example.base.FundUserBaseExample;
 import com.cmj.example.mapper.FundUserBaseMapper;
 import com.cmj.example.strategy.reader.DataReader;
 import com.cmj.example.utils.CollectionUtils;
@@ -14,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,7 +31,38 @@ public class FundUserDataImportChain extends AbstractDataImportChain<FundUserBas
 
     @Override
     protected List<FundUserBase> parseFromTarget(String content) {
-        return null;
+        // 所有基金类型
+        List<FundUserBase> allUserList = fundUserBaseMapper.selectByExample(new FundUserBaseExample().createCriteria()
+                .andIsDeleteEqualTo(0)
+                .example());
+        try {
+            JSONArray diffList = JSONObject.parseObject(content).getJSONArray("data");
+            List<FundUserBase> fundUserBaseList = new ArrayList<>(10);
+            for (int i = 0; i < diffList.size(); i++) {
+                JSONObject diffVo = diffList.getJSONObject(i);
+                String fundUserName = diffVo.getString("Name");
+                Date beginTime = diffVo.getDate("BDate");
+
+                // 数据基金经理已存在，跳过
+                if (allUserList.stream().anyMatch(fundUserBase -> fundUserBase.getName().equals(fundUserName))) {
+                    continue;
+                }
+                // 当前基金经理已经在保存列表中，跳过
+                if (fundUserBaseList.stream().anyMatch(fundUserBase -> fundUserBase.getName().equals(fundUserName))) {
+                    continue;
+                }
+
+                FundUserBase addBase = FundUserBase.builder()
+                        .name(fundUserName)
+                        .beginTime(beginTime)
+                        .build();
+                fundUserBaseList.add(addBase);
+            }
+            return fundUserBaseList;
+        } catch (Exception e) {
+            logger.error("FundUserDataImportChain.parseFromTarget.exception.is----->", e);
+            throw new RuntimeException("基金经理数据转换异常");
+        }
     }
 
     @Override
@@ -42,46 +72,4 @@ public class FundUserDataImportChain extends AbstractDataImportChain<FundUserBas
                     .forEach(fundUserBases -> fundUserBaseMapper.batchInsertSelective(fundUserBases, FundUserBase.Column.name, FundUserBase.Column.beginTime));
         }
     }
-
-//    @Override
-//    protected List<FundTypeBase> parseFromTarget(String content) {
-//        // 所有基金类型
-//        List<FundTypeBase> allTypeList = fundTypeBaseMapper.selectByExample(new FundTypeBaseExample().createCriteria()
-//                .andIsDeleteEqualTo(0)
-//                .example());
-//        try {
-//            JSONArray diffList = JSONObject.parseObject(content).getJSONArray("data");
-//            List<FundTypeBase> fundTypeBaseList = new ArrayList<>(10);
-//            for (int i = 0; i < diffList.size(); i++) {
-//                JSONObject diffVo = diffList.getJSONObject(i);
-//                String fundType = diffVo.getString("jjlx");
-//
-//                // 数据基金类型已存在，跳过
-//                if (allTypeList.stream().anyMatch(fbt -> fbt.getName().equals(fundType))) {
-//                    continue;
-//                }
-//                // 当前基金类型已经在保存列表中，跳过
-//                if (fundTypeBaseList.stream().anyMatch(fbt -> fbt.getName().equals(fundType))) {
-//                    continue;
-//                }
-//
-//                FundTypeBase addBase = FundTypeBase.builder()
-//                        .name(fundType)
-//                        .build();
-//                fundTypeBaseList.add(addBase);
-//            }
-//            return fundTypeBaseList;
-//        } catch (Exception e) {
-//            logger.error("FundTypeDataImportChain.parseFromTarget.exception.is----->", e);
-//            throw new RuntimeException("基金类型数据转换异常");
-//        }
-//    }
-//
-//    @Override
-//    public void saveData(List<FundTypeBase> date) {
-//        if (CollectionUtils.isNotNullAndEmpty(date)) {
-//            CollectionUtils.subList(date, 200)
-//                    .forEach(fundTypeBases -> fundTypeBaseMapper.batchInsertSelective(fundTypeBases, FundTypeBase.Column.name));
-//        }
-//    }
 }
