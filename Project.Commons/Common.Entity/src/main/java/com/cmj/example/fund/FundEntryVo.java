e361bbbffd8ecb01c17bf2a04100ca73cdff430c
mengjie_chen <1737756104@qq.com>
2021-01-06 20:10:38 +0800

    添加基金持仓明细信息

---
+++ Project.Commons/Common.Entity/src/main/java/com/cmj/example/fund/FundEntryVo.java
@@ -0,0 +1,96 @@
+package com.cmj.example.fund;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author mengjie_chen
+ * @description
+ * @date 2021/1/6
+ */
+@Data
+public class FundEntryVo {
+
+    /**
+     * 购买时间
+     */
+    private String createTime;
+    /**
+     * 股票代码
+     */
+    private String stockNumber;
+    /**
+     * 股票名称
+     */
+    private String stockName;
+    /**
+     * 股票持仓数量（单位：股）
+     */
+    private Integer stockBuyQuantity;
+    /**
+     * 股票持仓金额（单位：元）
+     */
+    private BigDecimal stockBuyAmount;
+    /**
+     * 基金名称
+     */
+    private String fundName;
+
+    public static final class FundEntryVoBuilder {
+        private String createTime;
+        private String stockNumber;
+        private String stockName;
+        private Integer stockBuyQuantity;
+        private BigDecimal stockBuyAmount;
+        private String fundName;
+
+        private FundEntryVoBuilder() {
+        }
+
+        public static FundEntryVoBuilder fundEntryVo() {
+            return new FundEntryVoBuilder();
+        }
+
+        public FundEntryVoBuilder createTime(String createTime) {
+            this.createTime = createTime;
+            return this;
+        }
+
+        public FundEntryVoBuilder stockNumber(String stockNumber) {
+            this.stockNumber = stockNumber;
+            return this;
+        }
+
+        public FundEntryVoBuilder stockName(String stockName) {
+            this.stockName = stockName;
+            return this;
+        }
+
+        public FundEntryVoBuilder stockBuyQuantity(Integer stockBuyQuantity) {
+            this.stockBuyQuantity = stockBuyQuantity;
+            return this;
+        }
+
+        public FundEntryVoBuilder stockBuyAmount(BigDecimal stockBuyAmount) {
+            this.stockBuyAmount = stockBuyAmount;
+            return this;
+        }
+
+        public FundEntryVoBuilder fundName(String fundName) {
+            this.fundName = fundName;
+            return this;
+        }
+
+        public FundEntryVo build() {
+            FundEntryVo fundEntryVo = new FundEntryVo();
+            fundEntryVo.setCreateTime(createTime);
+            fundEntryVo.setStockNumber(stockNumber);
+            fundEntryVo.setStockName(stockName);
+            fundEntryVo.setStockBuyQuantity(stockBuyQuantity);
+            fundEntryVo.setStockBuyAmount(stockBuyAmount);
+            fundEntryVo.setFundName(fundName);
+            return fundEntryVo;
+        }
+    }
+}
--- Project.Service/FundService/src/main/java/com/cmj/example/strategy/chain/FundEntryImportChain.java
+++ Project.Service/FundService/src/main/java/com/cmj/example/strategy/chain/FundEntryImportChain.java
@@ -3,17 +3,20 @@ package com.cmj.example.strategy.chain;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.cmj.example.base.*;
+import com.cmj.example.fund.FundEntryVo;
 import com.cmj.example.mapper.FundBaseMapper;
 import com.cmj.example.mapper.FundEntryBaseMapper;
 import com.cmj.example.mapper.FundTypeBaseMapper;
+import com.cmj.example.mapper.StockBaseMapper;
 import com.cmj.example.strategy.reader.DataReader;
 import com.cmj.example.utils.CollectionUtils;
 import com.cmj.example.utils.SpringContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author mengjie_chen
@@ -26,6 +29,7 @@ public class FundEntryImportChain extends AbstractDataImportChain<FundEntryBase>
     private final FundBaseMapper fundBaseMapper = SpringContextHolder.getBean(FundBaseMapper.class);
     private final FundTypeBaseMapper fundTypeBaseMapper = SpringContextHolder.getBean(FundTypeBaseMapper.class);
     private final FundEntryBaseMapper fundEntryBaseMapper = SpringContextHolder.getBean(FundEntryBaseMapper.class);
+    private final StockBaseMapper stockBaseMapper = SpringContextHolder.getBean(StockBaseMapper.class);
 
     public FundEntryImportChain(DataReader reader) {
         super(reader);
@@ -42,11 +46,44 @@ public class FundEntryImportChain extends AbstractDataImportChain<FundEntryBase>
                 .andIsDeleteEqualTo(0)
                 .example());
         try {
-            JSONArray diffList = JSONObject.parseObject(content).getJSONArray("data");
+            JSONArray diffList = JSONObject.parseObject(content).getJSONObject("rsm").getJSONArray("data");
             List<FundBase> fundBaseList = new ArrayList<>(10);
+            List<FundEntryVo> fundEntryVoList = new ArrayList<>(10);
             for (int i = 0; i < diffList.size(); i++) {
                 JSONObject diffVo = diffList.getJSONObject(i);
-                String fundName = diffVo.getString("sname");
+                String createTime = diffVo.getString("date");
+                String stockNumber = diffVo.getJSONObject("items").getString("symbol");
+                String stockName = diffVo.getJSONObject("items").getString("name");
+                Integer stockBuyQuantity = diffVo.getJSONObject("items").getInteger("sh_nums");
+                BigDecimal stockBuyAmount = diffVo.getJSONObject("items").getBigDecimal("sh_marketvalue");
+                String fundName = diffVo.getJSONObject("items").getString("sh_name");
+
+                FundEntryVo fundEntryVo = FundEntryVo.FundEntryVoBuilder.fundEntryVo()
+                        .createTime(createTime)
+                        .stockNumber(stockNumber.substring(2))
+                        .stockName(stockName)
+                        .stockBuyQuantity(stockBuyQuantity)
+                        .stockBuyAmount(stockBuyAmount)
+                        .fundName(fundName)
+                        .build();
+
+                fundEntryVoList.add(fundEntryVo);
+            }
+
+            // 所有股票代码
+            List<String> stockNumberList = fundEntryVoList.stream()
+                    .map(FundEntryVo::getStockNumber)
+                    .distinct()
+                    .collect(Collectors.toList());
+            // key：股票代码     value：base
+            Map<String, Optional<StockBase>> stockNumberMap = stockBaseMapper.selectByExample(new StockBaseExample().createCriteria()
+                    .andIsDeleteEqualTo(0)
+                    .andNumberIn(stockNumberList)
+                    .example())
+                    .stream()
+                    .collect(Collectors.groupingBy(StockBase::getName, Collectors.reducing((t, t2) -> t)));
+
+            /*String fundName = diffVo.getString("sname");
                 String fundNumber = diffVo.getString("symbol");
                 String fundType = diffVo.getString("jjlx");
 
@@ -69,8 +106,7 @@ public class FundEntryImportChain extends AbstractDataImportChain<FundEntryBase>
                                 .orElse(FundTypeBase.builder().id(-1).build())
                                 .getId())
                         .build();
-                fundBaseList.add(addBase);
-            }
+                fundBaseList.add(addBase);*/
             return null;
         } catch (Exception e) {
             logger.error("FundDataImportChain.parseFromTarget.exception.is----->", e);
